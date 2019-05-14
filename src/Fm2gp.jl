df2c52f61f1df5244cbad785b55485118585dc55
Kristoffer Andersson <kode.kristoff@gmail.com>
2019-05-14 16:40:18 +0200

    add tests

--- src/Fm2gp.jl
+++ src/Fm2gp.jl
@@ -1,5 +1,23 @@
 module Fm2gp
 
-greet() = print("Hello World!")
+export is_odd, half, multiply1
+
+function is_odd(x)::Bool
+    x & 0x1
+end
+
+function half(x)
+    x >> 1
+end
+
+function multiply1(n, a)
+    if n == one(n)  return a
+    end
+
+    b = multiply1(half(n), a*a)
+    if is_odd(n)    return b * a
+    else            return b
+    end
+end
 
 end # module
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+ using Fm2gp, Test
+
+@testset "is_odd" begin
+    @test is_odd(2) == false
+end
