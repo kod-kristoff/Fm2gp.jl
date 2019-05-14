c104b0e575e592df160317a8bc72e63c71110a87
Kristoffer Andersson <kode.kristoff@gmail.com>
2019-05-14 17:22:36 +0200

    testing

--- src/Fm2gp.jl
+++ src/Fm2gp.jl
@@ -1,6 +1,6 @@
 module Fm2gp
 
-export is_odd, half, multiply1
+export is_odd, half, multiply_recursive
 
 function is_odd(x)::Bool
     x & 0x1
@@ -10,14 +10,13 @@ function half(x)
     x >> 1
 end
 
-function multiply1(n, a)
+function multiply_recursive(n, a)
     if n == one(n)  return a
     end
 
-    b = multiply1(half(n), a*a)
-    if is_odd(n)    return b * a
-    else            return b
-    end
+    result = multiply1(half(n), a*a)
+    if is_odd(n)    result += a end
+    result
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,22 @@
 
 @testset "is_odd" begin
     @test is_odd(2) == false
+    @test is_odd(3) == true
+end
+
+@testset "half" begin
+    @test half(4) == 2
+    @test half(7) == 3
+end
+
+@testset "multiply_recursive" begin
+    n, x = 1, 2
+    r = multiply_recursive(n, x)
+    @test r == x
+    @test typeof(r) == typeof(x)
+
+    n, x = 3, 4.0
+    r = multiply_recursive(n, x)
+    @test r == 12.0
+    @test typeof(r) == typeof(x)
 end
